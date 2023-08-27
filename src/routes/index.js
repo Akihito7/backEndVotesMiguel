f8c5ff43f4f4444a6cb8725aa1aaa32e1f6e48dc
akihito7 <akihitopro7@gmail.com>
2023-08-26 21:49:44 -0300

    oi

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,3 +1,5 @@
+const knex = require("../database");
+
 const { Router } = require("express");
 //const routesWishes = require("./routesWhises");
 //const routesWishesNotConfirmed = require("./routesWishesNotConfirmed");
@@ -6,7 +8,14 @@ const routes = Router();
 
 //routes.use('/wishes', routesWishes );
 //routes.use('/wishesnotconfirmed', routesWishesNotConfirmed );
-routes.get("/jubileu", (req,res) => {
+routes.get("/jubileu", async (req,res) => {
+
+    await knex("peoples_confirmed").insert({
+        name: "ricardo",
+        address: "osvaldo",
+        school: "leno",
+        schoolCouncil: "leno"
+    })
     res.status(200).json("cheguei")
 })
 
