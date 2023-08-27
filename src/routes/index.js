2a0c0283bb8e0eda622ed75ef91b355c691e1d9b
akihito7 <akihitopro7@gmail.com>
2023-08-26 21:45:54 -0300

    first commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
 const { Router } = require("express");
-const routesWishes = require("./routesWhises");
-const routesWishesNotConfirmed = require("./routesWishesNotConfirmed");
+//const routesWishes = require("./routesWhises");
+//const routesWishesNotConfirmed = require("./routesWishesNotConfirmed");
 
 const routes = Router();
 
-routes.use('/wishes', routesWishes );
-routes.use('/wishesnotconfirmed', routesWishesNotConfirmed );
-
+//routes.use('/wishes', routesWishes );
+//routes.use('/wishesnotconfirmed', routesWishesNotConfirmed );
+routes.get("/jubileu", (req,res) => {
+    res.status(200).json("cheguei")
+})
 
 module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -10,5 +10,6 @@ app.use(cors());
 const routes = require('./routes');
 app.use(routes)
 
+
 const PORT = 7777;
 app.listen(PORT, () => console.log("Server running on port", PORT));
